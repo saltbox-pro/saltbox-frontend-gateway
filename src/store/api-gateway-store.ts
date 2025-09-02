60b16f8bcbbd7e99b32ddfbe5981ed34c24cde8b
Igor Eliseev <i.eliseev@saltbox.pro>
2025-09-02 12:30:24 +0300

    fix: remove env store

--- src/store/api-gateway-store.ts
+++ src/store/api-gateway-store.ts
@@ -3,14 +3,12 @@ import {
   DiscoveryApi,
   Configuration,
 } from "@saltbox/saltbox-gateway-api-client";
-import { appStore, envStore } from "saltbox-gateway/store";
+import { appStore } from "saltbox-gateway/store";
 
 class ApiGatewayStore {
   private get apiConfig() {
-    if (!envStore.env || !appStore.authStore?.user?.access_token)
-      return undefined;
+    if (!appStore.authStore?.user?.access_token) return undefined;
     return new Configuration({
-      basePath: envStore.env.api_base_path,
       headers: {
         Authorization: `Bearer ${appStore.authStore.user.access_token}`,
       },
--- src/store/index.ts
+++ src/store/index.ts
@@ -1,3 +1,2 @@
-export { envStore } from "./env-store";
 export { appStore } from "./app-store";
 export { apiGatewayStore } from "./api-gateway-store";
