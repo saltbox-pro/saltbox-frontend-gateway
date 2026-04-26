6ad0d1a04a549c5ab2c1a98c8cd8c2bd45b31118
Igor Eliseev <i.eliseev@saltbox.pro>
2026-04-26 21:00:24 +0300

    feat: rework webpack config for add hash-named chunks and change cache politics in nginx conf

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,37 @@
-const { merge } = require("webpack-merge");
+const path = require("path");
+
+const CopyPlugin = require("copy-webpack-plugin");
 const webpack = require("webpack");
 const singleSpaDefaults = require("webpack-config-single-spa-react-ts");
-const path = require("path");
-const CopyPlugin = require("copy-webpack-plugin");
+const { merge } = require("webpack-merge");
+
+class EmitEntryShimPlugin {
+  constructor(opts) {
+    this.shimName = opts.shimName;
+    this.entryName = opts.entryName || "main";
+  }
+  apply(compiler) {
+    const pluginName = "EmitEntryShimPlugin";
+    compiler.hooks.thisCompilation.tap(pluginName, (compilation) => {
+      compilation.hooks.processAssets.tap(
+        { name: pluginName, stage: compiler.webpack.Compilation.PROCESS_ASSETS_STAGE_SUMMARIZE },
+        () => {
+          const entrypoint = compilation.entrypoints.get(this.entryName);
+          if (!entrypoint) return;
+          const entryChunk = entrypoint.getEntrypointChunk();
+          const jsFile = [...entryChunk.files].find((f) => f.endsWith(".js"));
+          if (!jsFile || jsFile === this.shimName) return;
+          const shim = `export * from "./${jsFile}";\n`;
+          compilation.emitAsset(this.shimName, new compiler.webpack.sources.RawSource(shim));
+        }
+      );
+    });
+  }
+}
 
 module.exports = (webpackConfigEnv, argv) => {
+  const isProd = argv.mode === "production";
+
   const defaultConfig = singleSpaDefaults({
     orgName: "saltbox",
     projectName: "gateway",
@@ -28,11 +55,35 @@ module.exports = (webpackConfigEnv, argv) => {
       }),
       new webpack.DefinePlugin({
         DEVELOPMENT: argv.mode === "development",
-        PRODUCTION: argv.mode === "production",
+        PRODUCTION: isProd,
       }),
-    ],
+      isProd && new EmitEntryShimPlugin({ shimName: "index.js" }),
+    ].filter(Boolean),
     output: {
-      filename: "index.js",
+      filename: isProd ? "index.[contenthash].js" : "index.js",
+      chunkFilename: "[name].[contenthash].js",
+      assetModuleFilename: "assets/[name].[contenthash][ext]",
+    },
+    optimization: {
+      moduleIds: "deterministic",
+      chunkIds: "deterministic",
+      runtimeChunk: false,
+      splitChunks: {
+        chunks: "async",
+        cacheGroups: {
+          defaultVendors: {
+            test: /[\\/]node_modules[\\/]/,
+            name: "vendors",
+            priority: -10,
+            reuseExistingChunk: true,
+          },
+          default: {
+            minChunks: 2,
+            priority: -20,
+            reuseExistingChunk: true,
+          },
+        },
+      },
     },
   });
 
