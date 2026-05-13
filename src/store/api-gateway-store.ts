d4aca74b2360ea1bc906cb14fe69cd2ceb15842c
Igor Eliseev <i.eliseev@saltbox.pro>
2026-05-13 03:15:58 +0300

    feat: integrate server error middleware

--- src/store/api-gateway-store.ts
+++ src/store/api-gateway-store.ts
@@ -1,3 +1,4 @@
+import { createServerErrorMiddleware } from "@saltbox/saltbox-frontend-common";
 import { DiscoveryApi, Configuration } from "@saltbox/saltbox-gateway-api-client";
 import { computed, makeObservable, observable } from "mobx";
 
@@ -15,6 +16,7 @@ class ApiGatewayStore {
       headers: {
         Authorization: `Bearer ${appStore.authStore.user.access_token}`,
       },
+      middleware: [createServerErrorMiddleware()],
     });
   }
 
